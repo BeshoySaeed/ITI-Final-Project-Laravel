396cf190c10e9ece5d0bca2134a757a847123867
MartinaMagdi <martinmagdi6@gmail.com>
2023-10-20 01:07:06 +0300

    Finishing customer service phones controller

---
+++ app/Http/Controllers/CustomerServicePhoneController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CustomerServicePhone;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CustomerServicePhoneRequest;
+use App\Rules\PhoneRule;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+class CustomerServicePhoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $customerServicePhone = CustomerServicePhone::all();
+        return response()->json([
+            "data" => $customerServicePhone,
+            'status' => 'success',
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "phone" => ["required", "regex:/^(\+20-1)[0-9]{9}$/", Rule::unique('customer_service_phones')],
+            "active"=>"required"
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 'failed',
+                'message' => $validator->errors()->all()
+            ], 422);
+        }
+
+        $customerServicePhone = CustomerServicePhone::create($request->all());
+        return response()->json([
+            "data" => $customerServicePhone,
+            'status' => 'success',
+            'message' => 'Customer service phone added successfully'
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CustomerServicePhone $customerServicePhone)
+    {
+        return $customerServicePhone;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CustomerServicePhone $customerServicePhone)
+    {
+        $validator = Validator::make($request->all(), [
+            'phone'=>['required', "regex:/^(\+20-1)[0-9]{9}$/", Rule::unique('customer_service_phones')->ignore($customerServicePhone->phone)],
+            "active"=>"required"
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 'failed',
+                'message' => $validator->errors()->all()
+            ], 422);
+        }
+
+        $customerServicePhone->update($request->all());
+        return response()->json([
+            "data" => $customerServicePhone,
+            'status' => 'success',
+            'message' => 'Customer service phone updated successfully'
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CustomerServicePhone $customerServicePhone)
+    {
+        $customerServicePhone->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Customer service phone deleted successfully'
+        ], 200);
+    }
+}
